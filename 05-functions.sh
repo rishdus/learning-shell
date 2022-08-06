e2c7551eb78c2eb2f925b440ac30520adb917f70
rishdus <risheek.dustin@gmail.com>
2022-08-06 14:53:06 +0530

    updating ./learning-shell

--- 05-functions.sh
+++ 05-functions.sh
@@ -3,8 +3,11 @@
 ## Declare a fuction
 sample() {
   echo sample function
+  echo a from main program = $a
+  b=20
 }
 
 ## Access a function simply function name
 sample
-
+a=10
+echo b from function = $b
