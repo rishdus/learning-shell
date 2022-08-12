bcb4fe98c9af7378f38c1300b1bf6c4cfc500f99
rishdus <risheek.dustin@gmail.com>
2022-08-12 10:51:16 +0530

    updating ./learning-shell

--- 07-inputs.sh
+++ 07-inputs.sh
@@ -6,3 +6,14 @@ echo First Argument = $1
 echo secong argument = $2
 echo All Argument = $*
 echo number of arguments = $#
+
+## Arguments in func are different to main func arguments
+
+sample() {
+  echo First Argument in function  = $1
+  echo secong argument in func  = $2
+  echo All Argument in func  = $*
+  echo number of arguments = $#
+}
+
+sample 100 200
